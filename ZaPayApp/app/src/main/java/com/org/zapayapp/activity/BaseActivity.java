ba3d777b4160141887a8af7a3f9d361c82f9f3b7
admin <ashok.webvillee@gmail.com>
2020-07-20 16:22:23 +0530

    start

---
+++ ZaPayApp/app/src/main/java/com/org/zapayapp/activity/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.org.zapayapp.activity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.org.zapayapp.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+}
---
+++ ZaPayApp/app/src/main/java/com/org/zapayapp/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.org.zapayapp.activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.org.zapayapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
